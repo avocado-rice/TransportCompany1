2cbba51f7e35e75c175deb16b76aa51dbad0f9c3
avocado-rice <nadiakingan@gmail.com>
2024-06-01 02:36:08 +0200

    tralala

--- CityStation.java
+++ CityStation.java
@@ -3,15 +3,15 @@ import java.util.Random;
 public class CityStation extends Station
 {
     Random random = new Random();
-    
+
     public CityStation(String name, int x, int y)
     {
         super(name, x, y);
     }
-    
+
     @Override
-    public void calculate_passengers(int min, int max)
+    public void generate_passengers()
     {
-        passengers += random.nextInt((max-min)+1) + min;
+        passengers += random.nextInt((250-50)+1) + 50; // min 50 max 250
     }
 }
